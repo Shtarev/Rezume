8d1cd2b7ddd96838169ef829d85b10b90f9ea0b4
Andrey Shtarev <54120707+Shtarev@users.noreply.github.com>
2024-11-09 21:33:26 +0100

    todo (#6)

--- view/block_8.php
+++ view/block_8.php
@@ -6,12 +6,12 @@ $block_8 = array_chunk($block_8, 9);
 foreach($block_8 as $key=>$value){
 	echo '<div class="row text-center">
 			  <div class="col-sm-4 col-12 p-0" style="margin-bottom: 20px"><a href="images/'.$block_8[$key][0].'" target="_blank"><img class="img-thumbnail"  src="images/'.$block_8[$key][1].'" alt=""></a><p>'.$block_8[$key][2].'</p></div>';
-			  
-			  if($block_8[$key][3]){
+			  // TODO: What the fuck did I do here 6 years ago???
+			  if(isset($block_8[$key][3])){
 			  echo
 			  '<div class="col-sm-4 col-12 p-0" style="margin-bottom: 20px"><a href="images/'.$block_8[$key][3].'" target="_blank"><img class="img-thumbnail"  src="images/'.$block_8[$key][4].'" alt=""></a><p>'.$block_8[$key][5].'</p></div>';
 			  }
-			  if($block_8[$key][6]){
+			  if(isset($block_8[$key][6])){
 			  echo
 			  '<div class="col-sm-4 col-12 p-0" style="margin-bottom: 20px"><a href="images/'.$block_8[$key][6].'" target="_blank"><img class="img-thumbnail"  src="images/'.$block_8[$key][7].'" alt=""></a><p>'.$block_8[$key][8].'</p></div>';
 			  }
